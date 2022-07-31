a306c273f3beb74c9f40755abb008d5a591742a8
Somin <sominupadhyaya@gmail.com>
2022-07-31 19:59:36 +0545

    start UseContextComp

---
+++ src/components/UseContextComp.tsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+
+const UseContextComp = () =>{
+    return (
+        <>
+        <div>
+            Hello from use context component 
+        </div>
+        </>
+    )
+}
+export default UseContextComp
