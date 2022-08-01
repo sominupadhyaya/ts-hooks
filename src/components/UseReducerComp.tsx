9bdf8c1a4f4b10c5b42af33c2bee21f792ad2893
Somin <sominupadhyaya@gmail.com>
2022-08-01 10:11:13 +0545

    complete UseReducerComp

--- src/components/UseReducerComp.tsx
+++ src/components/UseReducerComp.tsx
@@ -1,7 +1,45 @@
+import { useReducer } from "react"
+
+const initialState = {
+    counter : 0
+}
+type ACTIONTYPES = 
+ | {type : "increment" , payload : number}
+ | {type : "decrement" , payload : number}
+
+const counterReducer = (state : typeof initialState , action : ACTIONTYPES) =>{
+    switch(action.type){
+        case "increment":
+            return {
+                ...state,
+                counter : state.counter + action.payload
+            }
+        case "decrement":
+            return {
+                ...state,
+                counter : state.counter - action.payload 
+            }
+        default :
+        throw new Error("Bad Action")
+    }
+}
+
+
 const UseReducerComp = () =>{
+    const [state, dispatch] = useReducer(counterReducer , initialState)
+    
     return (
         <>
-        
+        <div>{state.counter}</div>
+        <button onClick={() => dispatch({
+            type : "increment",
+            payload : 100
+        })}>Increment</button>
+
+        <button onClick={() =>dispatch({
+            type : "decrement", 
+            payload : 50 
+        })}>Decrement</button>
         </>
     )
 }
